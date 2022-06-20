b798f26d155f6e8e42a18fb00c5f4e5d68ee1327
caidy <vcsdk2468@gmail.com>
2022-06-20 22:01:26 +0800

    modify

--- src/main/java/projectbasic/config/OpenApiConfig.java
+++ src/main/java/projectbasic/config/OpenApiConfig.java
@@ -1,46 +1,47 @@
 package projectbasic.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "SourceFX"))
-@SecurityScheme(
-        name = "user",
-        type = SecuritySchemeType.HTTP,
-        scheme = "Bearer",
-        paramName = HttpHeaders.AUTHORIZATION,
-        in = SecuritySchemeIn.HEADER,
-        bearerFormat = "accessToken"
-)
 public class OpenApiConfig {
 
     @Bean
-    public GroupedOpenApi defaultApiGroup() {
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("projectbasic")
+                        .summary("projectbasic")
+                        .version("1.0.0-SNAPSHOT")
+                )
+                .schemaRequirement("user", new SecurityScheme()
+                        .type(SecurityScheme.Type.APIKEY)
+                        .name(HttpHeaders.AUTHORIZATION)
+                        .in(SecurityScheme.In.HEADER)
+                );
+    }
+
+    @Bean
+    public GroupedOpenApi defaultApi() {
         return GroupedOpenApi.builder()
                 .group("default")
                 .displayName("Default APIs")
                 .pathsToMatch("/api/**")
                 .addOperationCustomizer((operation, handlerMethod) -> {
-                    operation.setSecurity(Collections.singletonList(new SecurityRequirement().addList("user")));
+                    operation.addSecurityItem(new SecurityRequirement().addList("user"));
                     return operation;
                 })
                 .build();
     }
 
     @Bean
-    public GroupedOpenApi publicApiGroup() {
+    public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
                 .group("public")
                 .displayName("Public APIs")
