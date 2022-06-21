98b6d30f54e6db74d4b9e8d234d51bd63d843645
caidy <vcsdk2468@gmail.com>
2022-06-21 17:31:25 +0800

    add ui project

---
+++ ui/src/views/profile/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">roles: {{ roles.join(',') }}</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Profile',
+  computed: {
+    ...mapGetters([
+      'name',
+      'roles'
+    ])
+  }
+}
+</script>
